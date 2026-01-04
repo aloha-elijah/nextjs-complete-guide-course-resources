aae4aff5c99bc97cefb10ea011b33144dc491f08
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2026-01-04 13:06:30 -0800

    Fetch news data from backend API in NewsPage
    Replaced static dummy news data with dynamic fetching from the backend API at http://localhost:8080/news. Added loading and error states to improve user experience. Also added a new data.db file to the backend.

--- code/04-data-fetching/01-starting-project/app/(content)/news/page.js
+++ code/04-data-fetching/01-starting-project/app/(content)/news/page.js
@@ -1,11 +1,49 @@
-import { DUMMY_NEWS } from '@/dummy-news';
+'use client';
+// import { DUMMY_NEWS } from '@/dummy-news';
 import NewsList from '@/components/news-list';
+import { useEffect, useState } from 'react';
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [newsData, setNewsData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      // Simulate data fetching
+      setIsLoading(true);
+      console.log('Fetching data from http://localhost:8080');
+      const response = await fetch('http://localhost:8080/news');
+      if (!response.ok) {
+        setIsLoading(false);
+        setError('Failed to fetch data');
+      }
+      const data = await response.json();
+      setIsLoading(false);
+      setNewsData(data);
+      console.log('Fetched data:', data);
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  let newContent;
+
+  if (newsData) {
+    newContent = <NewsList news={newsData} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newContent}
     </>
   );
 }
